c4db13d2ab03b45bc3d151ca686a0d7150d49411
Devon <devon4329@proton.me>
2025-09-25 05:09:58 +0000

    Update and getting ready for submission

--- homework1/tests/test_task7.py
+++ homework1/tests/test_task7.py
@@ -2,9 +2,8 @@
 
 import pytest
 import sys
-import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(0, "../src")
 
 from task7 import calculate
 
